ed9a97fd4af7bbd7edb34ac6ba1873293913c786
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-10-26 14:34:44 -0400

    Extracting Methods Phase 3

--- src/com/chelsea/Main.java
+++ src/com/chelsea/Main.java
@@ -7,15 +7,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //For each question we want to validate the value that the user enters. If invalid we want to ask the same question again.
-        //const variables
-        final byte monthsInAYear = 12;
-        final byte percentage = 100;
-
         //these were removed out of the code block so it is able to be accessed globally
         int principal = 0;
-        float monthlyInterest = 0;
-        int numOfPayments = 0;
+        float annualInterest = 0;
+        byte years = 0;
 
         //created scanner obj & imported up top automatically as well
         Scanner scanner = new Scanner(System.in);
@@ -30,29 +25,42 @@ public class Main {
 
         while (true) {
             System.out.print("Annual Interest Rate: ");
-            float annualInterest = scanner.nextFloat();
-            if (annualInterest >= 1 && annualInterest <= 30) {
-                monthlyInterest = annualInterest / percentage / monthsInAYear;
+            annualInterest = scanner.nextFloat();
+            if (annualInterest >= 1 && annualInterest <= 30)//if valid value, calculate monthlyinterest then break out of the loop if not print error
                 break;
-            }
             System.out.println("Enter a value between 1 and 30");
         }
 
         while (true) {
             System.out.print("Period: (Years): ");
-            byte years = scanner.nextByte();
-            if (years >= 1 && years <= 30) {
-                numOfPayments = years * monthsInAYear;
+            years = scanner.nextByte();
+            if (years >= 1 && years <= 30) //if valid calculate numofpayments then break, if not print error
                 break;
-            }
             System.out.println("Enter a value between 1 and 30.");
         }
 
-        //this will work the same because consts were placed outside of local scope (inside code block) to outside of it!
-        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numOfPayments)) / (Math.pow(1 + monthlyInterest, numOfPayments) - 1);
-
-        //This is utilizing NumberFormat class to format it as currency
+        double mortgage = calculateMortgage(principal, annualInterest, years);
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Mortgage: " + mortgageFormatted);
     }
+
+    public static double calculateMortgage(
+            int principal,
+            float annualInterest,
+            byte years) {
+
+        //all the code to calculate the mortgage is in one location now
+        final byte monthsInAYear = 12;
+        final byte percentage = 100;
+
+        float monthlyInterest = annualInterest / percentage / monthsInAYear;
+        short numOfPayments = (short) (years * monthsInAYear);
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numOfPayments))
+                / (Math.pow(1 + monthlyInterest, numOfPayments) - 1);
+
+        return mortgage;
+    }
 }
+
